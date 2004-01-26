c5ec5b9e7c04a183d28c353d6c057becf07bb7a6
rdmurray <>
2004-01-26 03:16:22 +0000

    Add some unit tests to make sure the debugging flag gets set and cleared appropriately when the Debugging function is called. I wrote these tests after discovering a bug by hand; best practice is to write unit tests to demonstrate bugs before fixing them, so I did.

---
+++ pub/test/testDebug.py
@@ -0,0 +1,61 @@
+from unittest import main, TestCase, TestSuite, makeSuite
+import pub
+
+
+class TestDebug(TestCase):
+
+    def setUp(self):
+        #this is one reason global variables are a bad idea:
+        #  they make unit testing difficult.
+        reload(pub)
+
+    def testDebugOn(self):
+        self.failIf(pub.debugging)
+        pub.Debugging()
+        self.failUnless(pub.debugging)
+
+    def testDebugOnLower(self):
+        self.failIf(pub.debugging)
+        pub.Debugging('on')
+        self.failUnless(pub.debugging)
+
+    def testDebugOnUpper(self):
+        self.failIf(pub.debugging)
+        pub.Debugging('ON')
+        self.failUnless(pub.debugging)
+
+    def testDebugOnMixed(self):
+        self.failIf(pub.debugging)
+        pub.Debugging('On')
+        self.failUnless(pub.debugging)
+
+    def testDebugOff(self):
+        self.failIf(pub.debugging)
+        pub.Debugging('off')
+        self.failIf(pub.debugging)
+
+    def testDebugOnOffLower(self):
+        self.failIf(pub.debugging)
+        pub.Debugging('on')
+        self.failUnless(pub.debugging)
+        pub.Debugging('off')
+        self.failIf(pub.debugging)
+
+    def testDebugOnOffUpper(self):
+        self.failIf(pub.debugging)
+        pub.Debugging('on')
+        self.failUnless(pub.debugging)
+        pub.Debugging('OFF')
+        self.failIf(pub.debugging)        
+
+    def testDebugOnOffMixed(self):
+        self.failIf(pub.debugging)
+        pub.Debugging('on')
+        self.failUnless(pub.debugging)
+        pub.Debugging('Off')
+        self.failIf(pub.debugging)        
+
+
+suite = TestSuite([makeSuite(TestDebug)])
+
+if __name__ == '__main__': main()
