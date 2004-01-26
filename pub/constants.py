e2ef0f01c306d37685c1c7e1bb8c141ee6e21cd1
rdmurray <>
2004-01-26 00:47:39 +0000

    Detabify.

--- pub/constants.py
+++ pub/constants.py
@@ -1,11 +1,11 @@
-#	constants.py			7/2/02 Lalo
+#   constants.py            7/2/02 Lalo
 #
-#	This module defines constants used by most other
-#	PUB modules.  You shouldn't mess with this file unless you
-#	really know what you're doing.
+#   This module defines constants used by most other
+#   PUB modules.  You shouldn't mess with this file unless you
+#   really know what you're doing.
 #
-#	Use this module with:
-#	from pub.constants import *
+#   Use this module with:
+#   from pub.constants import *
 
 the = 'the'
 a = 'a'
@@ -18,10 +18,9 @@ FINISH = 2
 RUNNING = 1
 QUIT = 0
 try:
-	# use booleans if on 2.2.1+
-	TRUE = True
-	FALSE = False
+    # use booleans if on 2.2.1+
+    TRUE = True
+    FALSE = False
 except NameError:
-	TRUE = 1
-	FALSE = 0
-
+    TRUE = 1
+    FALSE = 0
